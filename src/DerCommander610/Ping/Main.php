3733e050ba4ff89001e50186e0a701960ddaef39
DerCommander620 <128488492+DerCommander620@users.noreply.github.com>
2023-06-26 19:38:24 +0200

    Update Main.php

--- src/DerCommander610/Ping/Main.php
+++ src/DerCommander610/Ping/Main.php
@@ -1,20 +1,29 @@
 <?php
 namespace DerCommander610\Ping;
 
-use DerCommander610\Ping\Commands\getPing;
+use pocketmine\command\CommandSender;
+use pocketmine\command\Command;
+use pocketmine\player\Player;
 use pocketmine\plugin\PluginBase;
-use pocketmine\utils\SingletonTrait;
+use pocketmine\Server;
 
 class Main extends PluginBase{
-    use SingletonTrait;
 
-    public function onLoad(): void{
-        self::setInstance($this);
-    }
-
-    public function onEnable(): void{
-        self::getServer()->getCommandMap()->registerAll($this->getName(), [
-            new getPing()
-        ]);
+    public function onCommand(CommandSender $sender, Command $command, string $label, array $args): bool{
+        switch($command->getName()){
+            case "getping":
+                if($sender instanceof Player){
+                    if($sender->hasPermission("ping.command.use")){
+                        if(isset($args[0])){
+                           $player = Server::getInstance()->getPlayerExact($args[0]);
+                           $sender->sendMessage("§a" . $player->getName() . " Ping: §e" . $player->getNetworkSession()->getPing());
+                    }
+                        $player = Server::getInstance()->getPlayerExact($args[0]);
+                        $sender->sendMessage("§aYour Ping: §e" . $player->getNetworkSession()->getPing());
+                    }
+                }
+                break;
+        }
+    return true;    
     }
 }
